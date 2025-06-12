d4277d3c4462543ee18f21fd76da3db947ca5c29
shivateymouri <shivateymouri.st@gmail.com>
2025-06-12 09:02:28 +0330

    footert text color fixed

--- src/components/Footer/page.jsx
+++ src/components/Footer/page.jsx
@@ -29,22 +29,22 @@ export default function page() {
       </div>
       <div className="w-full md:w-1/3 p-3 flex flex-col justify-center items-center gap-3">
       <h2 className="font-bold text-[18px]">شرکت خانومی</h2>
-      <h6 className="hover:text-pink-500 cursor-pointer">تماس با ما</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">درباره ما</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">فرصت های شغلی</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">حریم خصوصی</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">مجله خانومی</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">خانومی بیزینس</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">خانومی TV</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">تماس با ما</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">درباره ما</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">فرصت های شغلی</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">حریم خصوصی</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">مجله خانومی</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">خانومی بیزینس</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">خانومی TV</h6>
       </div>
       <div className="w-full md:w-1/3 p-3 flex flex-col justify-center items-center gap-3">
       <h2 className="font-bold text-[18px]">خدمات مشتریان</h2>
-      <h6 className="hover:text-pink-500 cursor-pointer">پرسش های متداول</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">راهنمای خرید و پرداخت</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">رویه های ارسال</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">شرایط مرجوعی</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">خرید کالای شانس</h6>
-      <h6 className="hover:text-pink-500 cursor-pointer">ارتباط با پشتیبانی</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">پرسش های متداول</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">راهنمای خرید و پرداخت</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">رویه های ارسال</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">شرایط مرجوعی</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">خرید کالای شانس</h6>
+      <h6 className="hover:text-pink-500 cursor-pointer text-gray-500">ارتباط با پشتیبانی</h6>
       </div>
     </footer>
   );
